edcd40d487b97617ac7fa0cdb53d2c2af3551109
surefire93 <121059685+j-stach@users.noreply.github.com>
2023-02-22 18:29:48 -0500

    minor

---
+++ rust_ntdirect/src/errors/mod.rs
@@ -0,0 +1,15 @@
+
+
+
+// contains error types for conversions and inputs
+
+// InstrumentStringError for invalid instrument argument, handles options returned by cstring
+// ArgumentOverflowError for bad conversions to C++ numeric types
+
+// also contains error types for handling bools returned by functions
+
+// InstrumentError
+// ConnectionError
+// OrderError
+
+// etc.
--- rust_ntdirect/src/lib.rs
+++ rust_ntdirect/src/lib.rs
@@ -1,3 +1,7 @@
-fn main() {
-    println!("Hello, world!");
-}
+
+
+use std::ffi::*;
+
+pub mod safe;
+pub mod raw;
+
