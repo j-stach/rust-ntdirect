b7f2bb98df73d93799f4b33951aed0d19457328e
surefire93 <121059685+j-stach@users.noreply.github.com>
2023-02-23 12:54:49 -0500

    added more fns

--- rust_ntdirect/src/safe.rs
+++ rust_ntdirect/src/safe.rs
@@ -10,6 +10,8 @@ enum MarketDataType {
     Bid,
 }
 
+// other custom types? OrderId etc that implement formatting
+
 // need unwrap errors handled
 // bool returns need to be success/fail errors?
 // would be good practice error handling, and also make interface more rusty
@@ -20,7 +22,7 @@ fn ask(instrument: &str, price: f64, size: i32) -> bool {
     let price: c_double = c_double::try_from(price).unwrap();
     let size: c_int = c_int::try_from(size).unwrap();
 
-    let result: c_int = unsafe { Ask(instrument.as_ptr(), price, size) };
+    let result: i32 = unsafe { Ask(instrument.as_ptr(), price, size) };
     match result {
         0 => return true,
         -1 => return false,
@@ -29,11 +31,66 @@ fn ask(instrument: &str, price: f64, size: i32) -> bool {
 
 }
 
+// needs a way to parse the timestamp as a string for the ffi (yyyyMMddHHmmss)
+// fn ask_playback(instrument: &str, price: f64, size: i32, timestamp:)
+
+fn avg_entry_price(instrument: &str, account: &str) -> f64 {
+    let instrument: CString = CString::new(instrument).unwrap();
+    let account: CString = CString::new(account).unwrap();
+    let result: f64 = unsafe { AvgEntryPrice(instrument.as_ptr(), account.as_ptr())};
+    return result
+}
+
+fn avg_fill_price(order_id: &str) -> f64 {
+    let order_id: CString = CString::new(order_id).unwrap();
+    let result: f64 = unsafe { AvgFillPrice(order_id.as_ptr())};
+    return result
+}
+
+fn bid(instrument: &str, price: f64, size: i32) -> bool {
+    let instrument: CString = CString::new(instrument).unwrap();
+    let price: c_double = c_double::try_from(price).unwrap();
+    let size: c_int = c_int::try_from(size).unwrap();
+
+    let result: i32 = unsafe { Bid(instrument.as_ptr(), price, size) };
+    match result {
+        0 => return true,
+        -1 => return false,
+        _ => panic!("Ask() returned an unexpected value"),
+    }
+}
+
+// needs a way to parse the timestamp as a string for the ffi (yyyyMMddHHmmss)
+// fn ask_playback(instrument: &str, price: f64, size: i32, timestamp:)
+
+fn buying_power(account: &str) -> f64 {
+    let account: CString = CString::new(account).unwrap();
+    let result: f64 = unsafe { BuyingPower(account.as_ptr()) };
+    return result   
+}
+
 fn cash_value(account: &str) -> f64 {
     let account: CString = CString::new(account).unwrap();
-    let result: c_double = unsafe { CashValue(account.as_ptr()) };
-    let result: f64 = f64::try_from(result).unwrap();
+    let result: f64 = unsafe { CashValue(account.as_ptr()) };
     return result
+}
+
+// needs order type & tif enums? look into valid commands before building this, may be able to make it easier
+// fn command(command: &str, account: &str, instrument: &str, action: &str, quantity: i32, order_type: &str, limit_price: f64, stop_price: f64, tif: &str, oco: &str, order_id: &str, strategy: &str, strategy_id: &str) -> bool {
+//     
+// }
+
+fn confirm_orders(confirm: bool) -> bool {
+    let confirm: c_int = match confirm {
+        true => 1,
+        false => 0,
+    };
+    let result: i32 = unsafe { ConfirmOrders(confirm) };
+    match result {
+        0 => return true,
+        -1 => return false,
+        _ => panic!("ConfirmOrders() returned an unexpected value"),
+    }
 }
 
 fn connected(show_message: bool) -> bool {
@@ -41,13 +98,35 @@ fn connected(show_message: bool) -> bool {
         true => 1,
         false => 0,
     }).unwrap();
-    let result: c_int = unsafe { Connected(show_message) };
+    let result: i32 = unsafe { Connected(show_message) };
     match result {
         0 => return true,
         -1 => return false,
         _ => panic!("Connected() returned an unexpected value"),
     }
 }
+
+fn filled(order_id: &str) -> i32 {
+    let order_id: CString = CString::new(order_id).unwrap();
+    let result: i32 = unsafe { Filled(order_id.as_ptr())};
+    return result
+}
+
+fn last(instrument: &str, price: f64, size: i32) -> bool {
+    let instrument: CString = CString::new(instrument).unwrap();
+    let price: c_double = c_double::try_from(price).unwrap();
+    let size: c_int = c_int::try_from(size).unwrap();
+
+    let result: i32 = unsafe { Last(instrument.as_ptr(), price, size) };
+    match result {
+        0 => return true,
+        -1 => return false,
+        _ => panic!("Last() returned an unexpected value"),
+    }
+}
+
+// needs a way to parse the timestamp as a string for the ffi (yyyyMMddHHmmss)
+// fn last_playback(instrument: &str, price: f64, size: i32, timestamp:)
 
 fn market_data(instrument: &str, market_data_type: MarketDataType) -> f64 {
     let instrument: CString = CString::new(instrument).unwrap();
@@ -56,21 +135,93 @@ fn market_data(instrument: &str, market_data_type: MarketDataType) -> f64 {
         Bid => 1,
         Ask => 2,
     }).unwrap();
-    let result: c_double = unsafe { MarketData(instrument.as_ptr(), data_type) };
-    let result: f64 = f64::try_from(result).unwrap();
+    let result: f64 = unsafe { MarketData(instrument.as_ptr(), data_type) };
+    return result
+}
+
+// enum for position?
+fn market_position(instrument: &str, account: &str) -> i32 {
+    let instrument: CString = CString::new(instrument).unwrap();
+    let account: CString = CString::new(account).unwrap();
+    let result: c_int = unsafe { MarketPosition(instrument.as_ptr(), account.as_ptr())};
+    let result: i32 = i32::try_from(result).unwrap();
+    return result
+}
+
+// needs to convert from pointer to i8 bytes to rust string, may want custom order_id type
+// fn new_order_id() -> String {
+//     let result: *const c_char = unsafe { NewOrderId()};
+//     let result: CString = CString::try_from(result).unwrap();
+//     let result: String = String::try_from(result);
+//     return result
+// }
+
+// needs to convert to rust string and parse into vecs
+// fn orders(account: &str) -> Vec<String> {}
+
+// needs to convert to rust string, may need custom types
+// fn order_status(order_id: &str) -> String {}
+
+fn realized_pnl(account: &str) -> f64 {
+    let account: CString = CString::new(account).unwrap();
+    let result: f64 = unsafe { RealizedPnL(account.as_ptr())};
+    return result
+}
+
+// check integer types for ports, unsigned?
+fn setup(host: &str, port: i32) -> bool {
+    let host: CString = CString::new(host).unwrap();
+    let port: c_int = c_int::try_from(port).unwrap();
+
+    let result: i32 = unsafe { SetUp(host.as_ptr(), port) };
+    match result {
+        0 => return true,
+        -1 => return false,
+        _ => panic!("Ask() returned an unexpected value"),
+    }
+}
+
+// need to convert to rust string and parse
+// fn get_stop_orders() -> Vec<String> {}
+
+// need to convert to rust string
+// fn strategies(account: &str) -> Vec<String> {}
+
+// enum like market position?
+fn strategy_position(strategy_id: &str) -> i32 {
+    let strategy_id: CString = CString::new(strategy_id).unwrap();
+    let result: i32 = unsafe { StrategyPosition(strategy_id.as_ptr()) };
     return result
 }
 
 fn subscribe_market_data(instrument: &str) -> bool {
     let instrument: CString = CString::new(instrument).unwrap();
-    let result: c_int = unsafe { SubscribeMarketData(instrument.as_ptr()) };
+    let result: i32 = unsafe { SubscribeMarketData(instrument.as_ptr()) };
     match result {
         0 => return true,
         -1 => return false,
-        _ => panic!("Ask returned an unexpected value"),
+        _ => panic!("SubscribeMarketData() returned an unexpected value"),
     }
 }
 
-// needs a way to parse the timestamp as a string for the ffi (yyyyMMddHHmmss)
-// fn ask_playback(instrument: &str, price: f64, size: i32, timestamp:)
+// need to convert to rust string
+// get_target_orders(strategy_id: &str) -> Vec<String> {}
 
+fn tear_down() -> bool {
+    let result: i32 = unsafe { TearDown() };    
+    match result {
+        0 => return true,
+        -1 => return false,
+        _ => panic!("SubscribeMarketData() returned an unexpected value"),
+    }
+}
+
+fn unsubscribe_market_data(instrument: &str) -> bool {
+    let instrument: CString = CString::new(instrument).unwrap();
+    let result: i32 = unsafe { UnsubscribeMarketData(instrument.as_ptr()) };
+    match result {
+        0 => return true,
+        -1 => return false,
+        _ => panic!("SubscribeMarketData() returned an unexpected value"),
+    }
+}
