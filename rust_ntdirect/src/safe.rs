6bc265cf6c2db717eb92ebd70f22e1d92411a6e8
surefire93 <121059685+j-stach@users.noreply.github.com>
2023-02-23 10:00:00 -0500

    adding functions

--- rust_ntdirect/src/safe.rs
+++ rust_ntdirect/src/safe.rs
@@ -1,7 +1,14 @@
-// use cxx::*;
 use std::ffi::*;
 
 use crate::raw::*;
+
+#[derive(Clone, Copy)]
+#[allow(dead_code, unused_variables)]
+enum MarketDataType {
+    Last,
+    Ask,
+    Bid,
+}
 
 // need unwrap errors handled
 // bool returns need to be success/fail errors?
@@ -17,9 +24,51 @@ fn ask(instrument: &str, price: f64, size: i32) -> bool {
     match result {
         0 => return true,
         -1 => return false,
-        _ => panic!("Ask returned an unexpected value"),
+        _ => panic!("Ask() returned an unexpected value"),
     }
 
+}
+
+fn cash_value(account: &str) -> f64 {
+    let account: CString = CString::new(account).unwrap();
+    let result: c_double = unsafe { CashValue(account.as_ptr()) };
+    let result: f64 = f64::try_from(result).unwrap();
+    return result
+}
+
+fn connected(show_message: bool) -> bool {
+    let show_message: c_int = c_int::try_from(match show_message {
+        true => 1,
+        false => 0,
+    }).unwrap();
+    let result: c_int = unsafe { Connected(show_message) };
+    match result {
+        0 => return true,
+        -1 => return false,
+        _ => panic!("Connected() returned an unexpected value"),
+    }
+}
+
+fn market_data(instrument: &str, market_data_type: MarketDataType) -> f64 {
+    let instrument: CString = CString::new(instrument).unwrap();
+    let data_type: c_int = c_int::try_from(match market_data_type {
+        Last => 0,
+        Bid => 1,
+        Ask => 2,
+    }).unwrap();
+    let result: c_double = unsafe { MarketData(instrument.as_ptr(), data_type) };
+    let result: f64 = f64::try_from(result).unwrap();
+    return result
+}
+
+fn subscribe_market_data(instrument: &str) -> bool {
+    let instrument: CString = CString::new(instrument).unwrap();
+    let result: c_int = unsafe { SubscribeMarketData(instrument.as_ptr()) };
+    match result {
+        0 => return true,
+        -1 => return false,
+        _ => panic!("Ask returned an unexpected value"),
+    }
 }
 
 // needs a way to parse the timestamp as a string for the ffi (yyyyMMddHHmmss)
